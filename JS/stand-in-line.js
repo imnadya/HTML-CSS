867326c78719a935ae9ef1c1d1ff5f72abab45cd
imnadya <111196866+imnadya@users.noreply.github.com>
2022-09-26 15:24:18 +0300

    Update stand-in-line.js

--- JS/stand-in-line.js
+++ JS/stand-in-line.js
@@ -4,22 +4,24 @@
 //The nextInLine function should then return the element that was removed.
 
 function nextInLine(arr, item) {
-  // Only change code below this line
-  var lastIndex=arr.length-1;
-  var removed=arr[0];
-  var last=arr[lastIndex];
-  console.log(removed);
-  console.log(last);
-  
-  return removed;
- // return item;
-  // Only change code above this line
+  // Your code here
+
+  var queue = arr.push(item);
+
+  var removeItem = arr.shift();
+
+  return removeItem;  // Change this line
 }
 
-// Setup
-const testArr = [1, 2, 3, 4, 5];
+// Test Setup
+var testArr = [1,2,3,4,5];
 
-// Display code
+// Display Code
 console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
 console.log("After: " + JSON.stringify(testArr));
+
+//Thinking
+// So I use .push() to add number at the end of the array. 
+// I use .shift() to remove the first element. 
+// Return the element was removed. return removeItem After I setup the var with the .shift() method.
